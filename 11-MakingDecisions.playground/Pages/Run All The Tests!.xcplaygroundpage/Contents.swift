31dcc79b7a17bc3206d241bb1b7819f469e10494
Tim Colson <timcolson@me.com>
2019-12-04 12:12:42 -0700

    Attempt to set the order correctly for all tests.

---
+++ 11-MakingDecisions.playground/Pages/Run All The Tests!.xcplaygroundpage/Contents.swift
@@ -0,0 +1,96 @@
+//: [Previous](@previous)
+//: - - -
+//: ![Run all the tests!](run_all_the_tests.jpg)
+//: ## Run all the Tests!
+//: 1. Uncomment a test case
+//: 2. Write the function
+//: 3. Make it pass all the tests!
+//: - - -
+//: ### Write function: "longer"
+
+
+
+//: ### Write function: "lengthOfLonger"
+
+
+
+//: ### Write function: "between"
+
+
+
+//: ### Write function: "sign"
+
+
+
+//: ### Write function: "weather"
+
+
+
+
+//: - - -
+
+import XCTest
+class DecisionTests: XCTestCase {
+    //
+    //    func testLonger() {
+    //        //
+    //        // longer(s1, s2) returns the longer of s1 and s2, both strings.
+    //        // If they are the same length, s1 is returned.
+    //        //
+    //        XCTAssertEqual("testing", longer("testing", "this"))
+    //        XCTAssertEqual("Mississipi", longer("Mississipi", "Arizona"))
+    //        XCTAssertEqual("argon", longer("neon","argon"))
+    //        XCTAssertEqual("abc", longer("abc", "xyz"))
+    //    }
+    //
+    //    func testLengthOfLonger() {
+    //        //
+    //        // lengthOfLonger(s1, s2) returns the length of the longer of s1 and s2,
+    //        // both strings.
+    //        //
+    //        XCTAssertEqual(7, lengthOfLonger("testing", "this"))
+    //        XCTAssertEqual(10, lengthOfLonger("Mississipi", "Arizona"))
+    //        XCTAssertEqual(5, lengthOfLonger("neon","argon"))
+    //        XCTAssertEqual(3, lengthOfLonger("abc", "xyz"))
+    //    }
+    //
+    //    func testBetween() {
+    //        //
+    //        // between takes three integers as arguments and returns true iff the
+    //        // first argument is numerically between the second and third arguments
+    //        ///
+    //        XCTAssertTrue(between(5, from: 1, through: 10))
+    //        XCTAssertFalse(between(5, from: 1, through: 3))
+    //        XCTAssertTrue(between(12, from: 1, through: 12))
+    //        XCTAssertTrue(between(100, from: 1, through: 100))
+    //        XCTAssertFalse(between(101, from: 1, through: 100))
+    //    }
+    //
+    //    func testSign() {
+    //        //
+    //        // sign(x) returns -1.0, 0.0, or 1.0 depending on whether
+    //        // x, a Double, is negative, zero, or positive.
+    //        //
+    //        XCTAssertEqual(-1.0, sign(-3.2))
+    //        XCTAssertEqual(0.0, sign(0.0))
+    //        XCTAssertEqual(1.0, sign(10.9))
+    //    }
+    //
+    //    func testWeather() {
+    //        // weather(temperature) returns a brief weather report based on the
+    //        // temperature, a Double.  It's cold if below 70, hot if 90 or more,
+    //        // but it's nice otherwise.
+    //        XCTAssertEqual("Cold!", weather(55))
+    //        XCTAssertEqual("Nice!", weather(70))
+    //        XCTAssertEqual("Nice!", weather(75))
+    //        XCTAssertEqual("Nice!", weather(89.99999999999))
+    //        XCTAssertEqual("Hot!", weather(90))
+    //        XCTAssertEqual("Hot!", weather(95))
+    //    }
+}
+
+//: ## Run the tests!
+DecisionTests.defaultTestSuite.run()
+
+//: [Next](@next)
+
