7796658d712fb0cc62ff8e5b86e8e9bac34d9b4f
Tim Colson <timcolson@me.com>
2019-12-04 11:24:07 -0700

    Add two pages to introduce test cases

---
+++ 11-MakingDecisions.playground/Pages/Testing WalkThrough.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+//: [Previous](@previous)
+//: - - -
+//: ![How do I do this?](raptor_howdoi.jpg)
+//: ## Testing Walk Through
+//: 1. Uncomment a test case
+//: 2. Write the function
+//: 3. Make it pass all the tests!
+//: - - -
+//: ### Write function: "absval"
+
+
+
+
+
+
+//: ### Write function: "larger"
+
+
+
+
+
+
+//: ## Test Cases
+import XCTest
+class DecisionTests: XCTestCase {
+//    func testAbsval() {
+//        //
+//        // absval(n) returns the absolute value of n, an integer
+//        // You may NOT use the abs() built-in function! :)
+//        //
+//        XCTAssertEqual(4, absval(4))
+//        XCTAssertEqual(4, absval(-4))
+//    }
+//    func testAbsvalThoroughly() {
+//        //
+//        // absval(n) returns the absolute value of n, an integer
+//        // You may NOT use the abs() built-in function! :)
+//        //
+//        XCTAssertEqual(4, absval(4))
+//        XCTAssertEqual(4, absval(-4))
+//        XCTAssertEqual(3, absval(3))
+//        XCTAssertEqual(3, absval(-3))
+//        XCTAssertEqual(0, absval(0))
+//
+//        let number = Int.random(in: 1 ..< 100)
+//        XCTAssertEqual(number, absval(number))
+//        XCTAssertEqual(number, absval(-number))
+//    }
+//    func testLarger() {
+//     //
+//     // larger(a,b) returns the larger of a and b, both integers
+//     //
+//     XCTAssertEqual(10, larger(3,10))
+//     XCTAssertEqual(19, larger(19,19))
+//     XCTAssertEqual(-10, larger(-30,-10))
+//     }
+    
+}
+
+//: ## Run the tests!
+DecisionTests.defaultTestSuite.run()
+
+
+//: [Next](@next)
